d09d02ed988bfa17be546bc2bd60d4cc3b0d7736
Cody Burley <codyburley@gmail.com>
2022-12-12 16:35:59 -0500

    added tablet and desktop rule styles

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -9,7 +9,7 @@ const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
     <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
       <div className="modal">
         <h2 className='modal__title'>RULES</h2>
-        <img src={rules} alt="Game rules" />
+        <img className='modal__image' src={rules} alt="Game rules" />
         <div className="modal-header">
           <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
             <span aria-hidden="true" className='modal__close'>&times;</span>
